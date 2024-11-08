1c732f6533c83e5d0009e57334d49793b7908204
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-08 10:05:24 +0100

    working JS | Name changes in HTML

--- order_form_cevapi/script.js
+++ order_form_cevapi/script.js
@@ -1,37 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () { // Ensures the function is called after Dom is loaded
-    getUserInput();
-});
+let submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click',foodCost);
 
-function getUserInput(){
-    // retrieves input from texboxes
-    let amountCevapi = document.getElementById('amountCevapi').value;
-    let amountSauce = document.getElementById('amountSauce').value;
+let cevapi = document.getElementById('amountCevapi');
+let sauce = document.getElementById('amountSauce');
 
-    // parse values to int
-    let amount1 = parseInt('amountCevapi');
-    let amount2 = parseInt('amountSauce');
+let delivery = document.getElementById('delivery');
+let totalCost = document.getElementById('totalCost');
 
-    // get the total cost
-    let totalCost = document.getElementById('totalCost');
+function foodCost(){
+    let total = +cevapi.value * 9 +sauce.value * 5;
 
-    // error handling
-    if(!isNaN(amount1) && !isNaN(amount2)) {
-        console.log("Both values are valid numbers. Proceeding with calculations.");
-        return {amount1, amount2};
-    } else {
-        console.log("One or more values are not numbers. Can not proceed.")
-        totalCost.innerHTML = `<h1>Make sure you insert a valid number</h1>`
-        return null;
+    if (delivery.checked){
+        total += 5;
     }
-
+    totalCost.innerHTML = `<h1>€ ${total}</h1>`;
 }
 
-function calcTotalCost() {
-    document.getElementById('submitBtn').addEventListener('click', function() {
-        const inputs = getUserInput();
-        if (inputs){
-            const {amount1, amount2} = inputs;
-            let totalCost = document.getElementById('totalCost')
-        }
-    })
-}
