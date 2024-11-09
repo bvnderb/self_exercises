2d1dd48b81557123f967584d141a8943e5bb63df
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-09 22:51:03 +0100

    working code

--- add_users_to_arrays/script.js
+++ add_users_to_arrays/script.js
@@ -7,6 +7,7 @@ const userList = document.getElementById('userList');
 
 addBtn.addEventListener('click', addUser);
 arrayBtn.addEventListener('click', arrayView);
+capitalize.addEventListener('click', capitalizeInput);
 
 function addUser() {
 
@@ -20,6 +21,28 @@ function addUser() {
     }
 
 }
+
+// function to capitalize the first letter from the input
+function capitalizeInput() {
+    const userTemp = userInput.value;
+    console.log("Add button clicked", userTemp);
+    
+    if (userTemp) { // Check if input is not empty
+        // Capitalize the first character and concatenate with the rest of the string
+        let capitalizedUser = `<span class="capitalized">${userTemp.charAt(0).toUpperCase()}</span>${userTemp}`;
+        
+        // Add the capitalized string to the array
+        userArray.push(capitalizedUser);
+
+        // Update the display with both original and capitalized text on the same line
+        userList.innerHTML += `${capitalizedUser}<br>`;
+        
+        // Clear input field after adding
+        userInput.value = '';
+    }
+}
+
+
 
 function arrayView() {
     console.log("ArrayView button clicked"); // verify the button works
