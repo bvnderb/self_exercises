cb405b0e6388a896e9287ba1eba44b6e20e73780
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-09 22:13:25 +0100

    working script + html with verify buttons

--- add_users_to_arrays/script.js
+++ add_users_to_arrays/script.js
@@ -1,7 +1,27 @@
-
 let userArray = [];
 
-addBtn.addEventListener('click', function(){
-    userArray.push(userInput.value);
-    userList.innerHTML += userArray;
-})
+const addBtn = document.getElementById('addBtn');
+const arrayBtn = document.getElementById('arrayBtn');
+const userInput = document.getElementById('userInput');
+const userList = document.getElementById('userList');
+
+addBtn.addEventListener('click', addUser);
+arrayBtn.addEventListener('click', arrayView);
+
+function addUser() {
+
+    const userTemp = userInput.value;
+    console.log("Add button clicked", userTemp);
+    console.log(userTemp);
+    if (userTemp) { // check if input is not empty
+        userArray.push(userTemp);
+        userList.innerHTML += userInput.value + `<br>`;
+        userInput.value = ''; // clear input field after adding
+    }
+
+}
+
+function arrayView() {
+    console.log("ArrayView button clicked"); // verify the button works
+    userList.innerHTML += userArray.toString();
+}
