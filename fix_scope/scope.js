c726d66de00d0f9ce9dba7bae944c7246bf81caa
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-12 22:24:07 +0100

    added comments for clarification

--- fix_scope/scope.js
+++ fix_scope/scope.js
@@ -1,15 +1,24 @@
 // Fix the scope issues in this code so the elves can track their toy production
+  function trackToyProduction() {
+    var totalToys = 100;
+    if(true) {
+        var totalToys = 50;  // This is causing issues!
+        let dailyToys = 10;
+    }
+    console.log(totalToys);  // Should print 100, not 50
+    console.log(dailyToys);  // Should cause an error - why?
+}
 
 // solution using const
 function trackToyProduction() {
-    const totalToys = 100;
-    const dailyToys = 10;
+    const totalToys = 100; // Moved both totalToys and dailyToys outside of the if statement to fix the issue
+    const dailyToys = 10; // Moved both totalToys and dailyToys outside of the if statement to fix the issue
     if (true) {
-        const totalToys = 50;  // This is causing issues! -> you can't overwrite a const because its a const
+        const totalToys = 50; // you can't overwrite a const
         
     }
-    console.log(totalToys);  // Should print 100, not 50
-    console.log(dailyToys);  // Should cause an error - why? - it's not defined?
+    console.log(totalToys);
+    console.log(dailyToys); // Doesn't cause an error because i declared it outside of the if
 }
 
 trackToyProduction();
