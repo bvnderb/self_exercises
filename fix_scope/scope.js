6e23e6d5d0d0ac0f47d2177cbd02078553135632
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-12 22:21:44 +0100

    provided additional solution

--- fix_scope/scope.js
+++ fix_scope/scope.js
@@ -1,6 +1,6 @@
 // Fix the scope issues in this code so the elves can track their toy production
 
-
+// solution using const
 function trackToyProduction() {
     const totalToys = 100;
     const dailyToys = 10;
@@ -10,6 +10,19 @@ function trackToyProduction() {
     }
     console.log(totalToys);  // Should print 100, not 50
     console.log(dailyToys);  // Should cause an error - why? - it's not defined?
+}
+
+trackToyProduction();
+
+// solution using let
+function trackToyProduction() {
+    let totalToys = 100; // Use let for block scope
+    if (true) {
+        let dailyToys = 10; // This is fine, as it's block-scoped
+        console.log(dailyToys); // This will correctly log 10
+    }
+    console.log(totalToys);  // This will correctly log 100
+    // console.log(dailyToys);  // Uncommenting this will cause an error because dailyToys is not accessible here
 }
 
 trackToyProduction();
