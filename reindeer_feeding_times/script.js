8f954cdf75414b5dad98ab7a2f0f332d88795ffb
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-13 13:40:49 +0100

    needs start over

---
+++ reindeer_feeding_times/script.js
@@ -0,0 +1,33 @@
+// Write an ES6 arrow function to schedule reindeer feeding times
+// The function should take the number of reindeer and return an array of feeding times
+
+const reindeer = ['Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen', 'Rudolph']
+const times = ['11:30', '11:40', '11:50', '12:00', '12:10', '12:20', '12:30', '12:40', '12:50']
+
+const feedReindeer = () => 
+
+
+
+
+
+// make array for reindeer:
+// 1.	Dasher
+// 2.	Dancer
+// 3.	Prancer
+// 4.	Vixen
+// 5.	Comet
+// 6.	Cupid
+// 7.	Donner
+// 8.	Blitzen
+// 9.   Rudolph
+
+// make array for feeding times:
+// 1.   11:30
+// 2.   11:40
+// 3.   11:50
+// 4.   12:00
+// 5.   12:10
+// 6.   12:20
+// 7.   12:30
+// 8.   12:40
+// 9.   12:50
