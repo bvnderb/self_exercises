9a4460f4e75d4b8c2442125f54b050f03dfc082e
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-27 23:02:46 +0100

    working V1

--- Pokemon_api/app.js
+++ Pokemon_api/app.js
@@ -1,18 +1,33 @@
-async function pokeFetch() {
+document.getElementById('regularButton').addEventListener('click', function () {
+    pokeFetch(false);
+});
+
+document.getElementById('shinyButton').addEventListener('click', function () {
+    pokeFetch(true);
+});
+
+async function pokeFetch(isShiny) {
     try {
         const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemonName}');
-        
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
         if (!response.ok) {
             throw new Error('No response received. Try again later');
-    }
-    
-    const data = await response.json();
-    console.log(data);
-    }
-catch (error) {
-    console.error('Error fetching user data:', error);
-}
-}
+        }
 
-pokeFetch();
+        const data = await response.json();
+        const pokemonSprite = isShiny ? data.sprites.front_shiny : data.sprites.front_default;
+
+
+        const imgElement = document.getElementById('pokemonSprite');
+
+
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+    }
+    catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+}
